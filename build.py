5384024226d1ccf491a1430531d3d1b094367770
ielm <leoni@rpi.edu>
2020-05-17 19:19:23 -0400

    add persuasion data

--- build.py
+++ build.py
@@ -6,16 +6,19 @@ import stanza
 import uuid
 import os
 
+from omicron.nlp import get_tokens
+
 
 ROOT_DIR = Path(f"{os.path.abspath(__file__)}").parent
-SRC_PATH = f"{ROOT_DIR}/data/src/AntiScam_annotated.txt"
+ANTISCAM_SRC_PATH = f"{ROOT_DIR}/data/src/AntiScam_annotated.txt"
+PERSUASION_SRC_PATH = f"{ROOT_DIR}/data/src/AntiScam_annotated.txt"
 RAW_DIALOGS = f"{ROOT_DIR}/data/dialogs/raw"
 ENHANCED_DIALOGS = f"{ROOT_DIR}/data/dialogs/enhanced"
 
 stanza_nlp = stanza.Pipeline('en')
 
 
-def build_data(input_filedir: str = SRC_PATH, _pprint: bool = False):
+def build_antiscam_data(input_file_dir: str = ANTISCAM_SRC_PATH, verbose: bool = False):
     header = ['turn', 'agent', 'text', 'tokens', 'intent', 'semantic_slot', 'topics']
     
     def _process_dialog(_data: list):
@@ -24,7 +27,7 @@ def build_data(input_filedir: str = SRC_PATH, _pprint: bool = False):
             print(".")
             _row = [el.strip() for el in _row.split('\t') if el != '']
             _row.insert(0, _index)
-            _row.insert(3, tokens(_row[2], False))
+            _row.insert(3, get_tokens(_row[2], False))
             _row.insert(6, topics(_row))
             return _row
 
@@ -37,7 +40,7 @@ def build_data(input_filedir: str = SRC_PATH, _pprint: bool = False):
                 _turn_index += 1
         return _dialog
 
-    with open(input_filedir, 'r', encoding="ISO-8859-1") as input_file:
+    with open(input_file_dir, 'r', encoding="ISO-8859-1") as input_file:
         dialog_index = 0 
         temp_dialog = []
         raw_dialogs = []
@@ -45,7 +48,7 @@ def build_data(input_filedir: str = SRC_PATH, _pprint: bool = False):
         for line in input_file:
             line = line.strip()
             if not line:
-                if temp_dialog != []:
+                if temp_dialog:
                     print(f"CONV{dialog_index}")
                     dialog_index += 1
                     raw_dialogs.append(temp_dialog)
@@ -55,6 +58,10 @@ def build_data(input_filedir: str = SRC_PATH, _pprint: bool = False):
             temp_dialog.append(line)
         # write_raw_files(raw_dialogs)  # UNCOMMENT THESE TO WRITE FILES
         # write_enhanced_files(dialogs)  # UNCOMMENT THESE TO WRITE FILES
+
+
+def build_persuasion_data(input_file_dir: str = PERSUASION_SRC_PATH, verbose: bool = False):
+    pass
 
 
 def write_enhanced_files(dialogs: list, directory: str = ENHANCED_DIALOGS, _pprint: bool = False):
@@ -76,28 +83,13 @@ def write_raw_files(dialogs: list, directory: str = RAW_DIALOGS, _pprint: bool =
                 file.write(f"{_r}\n\n")
 
 
-def tokens(text: str, _pprint: bool = False):
-    text = text.strip('"')
-    _tokens = {}
-    for s in stanza_nlp(f"{text}").sentences:
-        if _pprint:
-            print(f"\nSentence: {s.text}")
-        for w in s.words:
-            _tokens[f"{w.lemma}"] = w.to_dict()
-            if _pprint:
-                print(f"\t{w.lemma}:")
-                for item in _tokens[w.lemma]:
-                    print(f"\t\t{item}:\t{_tokens[w.lemma][item]}")
-    return _tokens
-
-
 def topics(turn: list):
     swords = set(stopwords.words("english"))
-    postags = ["NN", "NNP", "NNS", "NNPS", "PRP", "VB", "VBG", "VBD", "VBN", "VBP"]
+    post_tags = ["NN", "NNP", "NNS", "NNPS", "PRP", "VB", "VBG", "VBD", "VBN", "VBP"]
     tkns = turn[3]
-    tpcs = [w for w in tkns if tkns[w]['text'] not in swords and tkns[w]['xpos'] in postags]
+    tpcs = [w for w in tkns if tkns[w]['text'] not in swords and tkns[w]['xpos'] in post_tags]
     return tpcs
 
 
 if __name__ == '__main__':
-    build_data()
+    build_antiscam_data()
